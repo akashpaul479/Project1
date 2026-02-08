b23dc8e4022ebf3d715cca900040e632f8c2b5d3
akashpaul479 <akashpaul4790@gmail.com>
2026-02-08 18:12:48 +0530

    main func

--- college_management_system/hybridsystem.go
+++ college_management_system/hybridsystem.go
@@ -11,26 +11,43 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// MySQLStudentRepo implements StudentRepository using MySQL
 type MySQLStudentRepo struct {
 	DB *sql.DB
 }
 
+// MongoDBStudentRepo implements StudentRepository using MongoDB
 type MongoDBStudentRepo struct {
 	Collection *mongo.Collection
 }
 
+// MySQLLecturerRepo implements LecturerRepository using MySQL
 type MySQLLecturerRepo struct {
 	DB *sql.DB
 }
 
+// MongoDBLecturerRepo implements LecturerRepository using MongoDB
 type MongoDBLecturerRepo struct {
 	Collection *mongo.Collection
 }
 
+// MySQLLibraryRepo implements LecturerRepository using MySQL
+type MySQLLibraryRepo struct {
+	DB *sql.DB
+}
+
+// MongoDBLibraryRepo implements LecturerRepository using MongoDB
+type MongoDBLibraryRepo struct {
+	Collection *mongo.Collection
+}
+
+// Database connection
+// ConnectMySQL establishes a connection to MySQL using the DSN
 func ConnectMySQL() (*sql.DB, error) {
 
 	db, err := sql.Open("mysql", os.Getenv("MYSQL_DSN"))
@@ -48,6 +65,7 @@ func ConnectMySQL() (*sql.DB, error) {
 	return db, nil
 }
 
+// ConnectMongo establishes a connection to MongoDB using the URI
 func ConnectMongo() (*mongo.Database, error) {
 
 	client, err := mongo.Connect(
@@ -62,6 +80,22 @@ func ConnectMongo() (*mongo.Database, error) {
 	return client.Database(os.Getenv("MONGO_DB")), nil
 }
 
+// Global context used for Redis operations
+var Ctx = context.Background()
+
+// ConnectRedis initializes and returns Redis client
+func ConnectRedis() *redis.Client {
+
+	// Create Redis client with address and DB
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+		DB:   0,
+	})
+	return rdb
+}
+
+// Repository Interface
+// StudentRepository defines CRUD operations for Student entities.
 type StudentRepository interface {
 	CreateStudent(student Student) (*Student, error)
 
@@ -74,6 +108,7 @@ type StudentRepository interface {
 	DeleteStudent(id int) error
 }
 
+// LecturerRepository defines CRUD operations for Lecturer entities.
 type LecturerRepository interface {
 	CreateLecturer(l Lecturer) (*Lecturer, error)
 
@@ -86,6 +121,22 @@ type LecturerRepository interface {
 	DeleteLecturer(id int) error
 }
 
+// LecturerRepository defines CRUD operations for Lecturer entities.
+type LibraryRepository interface {
+	CreateLibrary(l Library) (*Library, error)
+
+	GetAllLibrary() ([]Library, error)
+
+	GetByIDLibrary(id int) (*Library, error)
+
+	UpdateLibrary(l Library) error
+
+	DeleteLibrary(id int) error
+}
+
+// Handler Repository Selectors
+
+// GetRepo selects the appropriate StudentRepository implementation
 func (h *StudentHandler) GetRepo(r *http.Request) StudentRepository {
 	dbType := r.URL.Query().Get("db")
 
@@ -96,6 +147,8 @@ func (h *StudentHandler) GetRepo(r *http.Request) StudentRepository {
 	// default
 	return h.MySQLRepo
 }
+
+// GetRepo selects the appropriate LecturerRepository implementation
 func (h *LecturerHandler) GetRepo(r *http.Request) LecturerRepository {
 	dbType := r.URL.Query().Get("db")
 
@@ -107,46 +160,83 @@ func (h *LecturerHandler) GetRepo(r *http.Request) LecturerRepository {
 	return h.MySQLRepo
 }
 
+// GetRepo selects the appropriate LibraryRepository implementation
+func (h *LibraryHandler) GetRepo(r *http.Request) LibraryRepository {
+	dbType := r.URL.Query().Get("db")
+
+	if dbType == "mongo" {
+		return h.MongoRepo
+	}
+
+	// default
+	return h.MySQLRepo
+}
+
+// College_Management_System initializes all services
+// and starts HTTP server
 func College_Management_System() {
+
+	// Load environment variables
 	godotenv.Load()
 
-	// Connect MySQl
+	// Initialize Redis client
+	redisClient := ConnectRedis()
+
+	// Connect to MySQl
 	mysqlDB, err := ConnectMySQL()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//  Connect MongoDB
+	// Connectto  MongoDB
 	mongodb, err := ConnectMongo()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Create repositories
 	mysqlStudent := NewMySQLStudentRepo(mysqlDB)
 	MongoStudent := NewMongoDBStudentRepo(mongodb.Collection("students"))
 
 	mysqlLecturer := NewMySQLLecturerRepo(mysqlDB)
 	mongoLecturer := NewMongoDBLecturerRepo(mongodb.Collection("lecturers"))
 
-	Studentshandler := &StudentHandler{MySQLRepo: mysqlStudent, MongoRepo: MongoStudent}
+	mysqlLibrary := NewMySQLLibraryRepo(mysqlDB)
+	mongoLibrary := NewMongoDBLibraryRepo(mongodb.Collection("library"))
 
-	Lecturershandler := &LecturerHandler{MySQLRepo: mysqlLecturer, MongoRepo: mongoLecturer}
+	// Create handlers with dependencies
+	Studentshandler := &StudentHandler{MySQLRepo: mysqlStudent, MongoRepo: MongoStudent, Redis: redisClient}
 
+	Lecturershandler := &LecturerHandler{MySQLRepo: mysqlLecturer, MongoRepo: mongoLecturer, Redis: redisClient}
+
+	Libraryshandler := &LibraryHandler{MySQLRepo: mysqlLibrary, MongoRepo: mongoLibrary, Redis: redisClient}
+
+	// Initialize router
 	r := mux.NewRouter()
 
+	// Student routes
 	r.HandleFunc("/students", Studentshandler.CreateStudent).Methods("POST")
 	r.HandleFunc("/students", Studentshandler.GetAllStudent).Methods("GET")
 	r.HandleFunc("/students/{id}", Studentshandler.GetByIDStudent).Methods("GET")
 	r.HandleFunc("/students/{id}", Studentshandler.UpdateStudent).Methods("PUT")
 	r.HandleFunc("/students/{id}", Studentshandler.DeleteStudent).Methods("DELETE")
 
+	// Lecturer routes
 	r.HandleFunc("/lecturers", Lecturershandler.CreateLecturer).Methods("POST")
 	r.HandleFunc("/lecturers", Lecturershandler.GetAllLecturer).Methods("GET")
 	r.HandleFunc("/lecturers/{id}", Lecturershandler.GetByIDLectuurer).Methods("GET")
 	r.HandleFunc("/lecturers/{id}", Lecturershandler.UpdateLecturer).Methods("PUT")
 	r.HandleFunc("/lecturers/{id}", Lecturershandler.DeleteLecturer).Methods("DELETE")
 
-	fmt.Println("Server running on port:8080")
+	// Library routes
+	r.HandleFunc("/libraries", Libraryshandler.CreateLibrary).Methods("POST")
+	r.HandleFunc("/libraries", Libraryshandler.GetAllLibrary).Methods("GET")
+	r.HandleFunc("/libraries/{id}", Libraryshandler.GetByIDLibrary).Methods("GET")
+	r.HandleFunc("/libraries/{id}", Libraryshandler.UpdateLibrary).Methods("PUT")
+	r.HandleFunc("/libraries/{id}", Libraryshandler.DeleteLibrary).Methods("DELETE")
 
+	// Start HTTP server
+	fmt.Println("Server running on port:8080")
 	http.ListenAndServe(":8080", r)
 
 }
